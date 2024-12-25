32971deaa684ba27e6447dfb91b7b905f152f9ac
Abdul-AR2908 <abdulrazaaqit@gmail.com>
2024-12-25 14:48:46 +0530

    feat:This is backend

---
+++ Samplebackend.php
@@ -0,0 +1,15 @@
+<?php
+    $connect = "I am backend";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>I am backend</h1>
+</body>
+</html>
